1201a0077b3c8fb2e522719bf14a1eacbc85b171
Ryan DeCook <ryandecook@gmail.com>
2019-02-02 11:48:31 -0600

    update api call start date

--- randysJobsWDC.js
+++ randysJobsWDC.js
@@ -102,7 +102,7 @@
   };
 
   myConnector.getData = function (table, doneCallback) {
-    $.getJSON("https://api.servicetitan.com/v1/jobs?filter.createdAfter=2018-06-01T00%3A00%3A00.000&filter.pageSize=10000&serviceTitanApiKey=8e9891b0-181b-4f74-9455-0195f66623d7", function (resp) {
+    $.getJSON("https://api.servicetitan.com/v1/jobs?filter.createdAfter=2018-01-01T00%3A00%3A00.000&filter.pageSize=10000&serviceTitanApiKey=8e9891b0-181b-4f74-9455-0195f66623d7", function (resp) {
       var feat = resp.data,
         tableData = [];
 
